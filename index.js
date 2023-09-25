cdbef0cbf519aeb28e919e392701a0087f34d85f
Madhur Goyal <mgoyal3@myseneca.ca>
2023-09-25 13:59:50 -0400

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,97 @@
+let element = document.querySelectorAll(".lines");
+let reset = document.querySelector(".reset");
+
+let count = 0;
+
+element.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerHTML === "") {
+      count % 2 === 0 ? (box.innerHTML = "X") : (box.innerHTML = "O");
+      count += 1;
+    }
+
+    checkWinner();
+  });
+});
+
+function checkWinner() {
+  if (
+    (element[0].innerHTML === "X" || element[0].innerHTML === "O") &&
+    element[0].innerHTML === element[1].innerHTML &&
+    element[1].innerHTML === element[2].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[3].innerHTML === "X" || element[3].innerHTML === "O") &&
+    element[3].innerHTML === element[4].innerHTML &&
+    element[4].innerHTML === element[5].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[6].innerHTML === "X" || element[6].innerHTML === "O") &&
+    element[6].innerHTML === element[7].innerHTML &&
+    element[7].innerHTML === element[8].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[0].innerHTML === "X" || element[0].innerHTML === "O") &&
+    element[0].innerHTML === element[4].innerHTML &&
+    element[4].innerHTML === element[8].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[2].innerHTML === "X" || element[2].innerHTML === "O") &&
+    element[2].innerHTML === element[4].innerHTML &&
+    element[4].innerHTML === element[6].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[0].innerHTML === "X" || element[0].innerHTML === "O") &&
+    element[0].innerHTML === element[3].innerHTML &&
+    element[3].innerHTML === element[6].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[1].innerHTML === "X" || element[1].innerHTML === "O") &&
+    element[1].innerHTML === element[4].innerHTML &&
+    element[4].innerHTML === element[7].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  } else if (
+    (element[2].innerHTML === "X" || element[2].innerHTML === "O") &&
+    element[2].innerHTML === element[5].innerHTML &&
+    element[5].innerHTML === element[8].innerHTML
+  ) {
+    disableAllButtons();
+    displayWinner();
+  }
+}
+
+function disableAllButtons() {
+  element.forEach((box) => {
+    box.setAttribute("disabled", true);
+  });
+}
+
+// Assuming 'reset' and 'element' are properly defined references
+reset.addEventListener("click", () => {
+  element.forEach((box) => {
+    box.removeAttribute("disabled");
+
+    box.innerHTML = "";
+  });
+  count = 0;
+});
+
+function displayWinner() {
+  count % 2 === 1
+    ? (document.querySelector(".winnerName").innerHTML = "Player(X) Wins")
+    : (document.querySelector(".winnerName").innerHTML = "Player(O) Wins");
+}
